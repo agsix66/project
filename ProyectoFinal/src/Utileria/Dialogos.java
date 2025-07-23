48b9bfa0e3a69a4cca7a5da153a19b5cb2f7f2b8
agsix66 <anelg277@gmail.com>
2025-07-22 19:00:35 -0500

    Delete ProyectoFinal directory

--- ProyectoFinal/src/Utileria/Dialogos.java
+++
@@ -1,9 +0,0 @@
-package Utileria;
-
-import javax.swing.*;
-
-public class Dialogos {
-    public static void alertesMensaje(String mensaje, String titulo , int tipo){
-        JOptionPane.showMessageDialog(null,mensaje,titulo,tipo);
-    }
-}
